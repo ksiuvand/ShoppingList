6467639faa9eb5e7c3b5079ff6989308a026b53a
ksiuvand <VandKsenia2016@yandex.ru>
2025-10-21 22:04:13 +0300

    implement domain layer

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.shoppinglist.domain
+
+class DeleteShopItemUseCase(
+    private val shopListRepository: ShopListRepository,
+) {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
